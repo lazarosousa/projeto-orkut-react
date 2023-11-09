29e543bd635a3353d36b147c452a7c7b01708955
Lazaro Sousa <129122838+lazarosousa@users.noreply.github.com>
2023-11-09 01:39:49 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import Login from "./components/pages/Login";
+//import Login from "./components/pages/Login";
+import Perfil from "./components/pages/Perfil";
 import "./styles/login.css";
 import "./styles/global.css";
 import "./styles/reset.css";
+import "./styles/Perfil.css";
+import "./styles/createProfile.css";
 
 function App() {
   return (
-   <Login />
+   <Perfil />
   );
 }
 
